0f3fbc3e0dab8ccf5adec46f833af5c6f1c699ca
Ranjani <sivaranjani.s92@gmail.com>
2019-08-11 09:00:18 -0700

    Update

--- src/com/anb/androidreference/Page_Test.java
+++ src/com/anb/androidreference/Page_Test.java
@@ -2,14 +2,14 @@ package com.anb.androidreference;
 
 
 import org.testng.annotations.Test;
-public class Page_Test extends Page_Helper{
+
+import com.androidreference.library.Base_Test;
+public class Page_Test extends Base_Test{
 
 Page_Helper helper = new Page_Helper();
 	
 	@Test
 	public void validateAndroidReferencePageElements() throws Exception {
-
-		helper.setup();
 		helper.android_Reference_Page_Elements();
 
 	}
