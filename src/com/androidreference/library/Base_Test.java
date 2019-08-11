686392891cfa8385ba6e83511da12d81770a5c0c
Ranjani <sivaranjani.s92@gmail.com>
2019-08-11 08:59:39 -0700

    Update

---
+++ src/com/androidreference/library/Base_Test.java
@@ -0,0 +1,87 @@
+package com.androidreference.library;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import com.anb.androidreference.Page_Helper;
+import com.anb.utilities.PropReader;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+
+
+public class Base_Test {
+	
+	private static Logger log = LogManager.getLogger(Page_Helper.class.getName());
+	String folder_name;
+	DateFormat df;
+
+
+	public AndroidDriver<MobileElement> driver;
+	
+	@BeforeClass
+	public void setup() throws IOException {
+		   System.out.println("Inside Before Method");
+		    PropReader.GetInstance(); 
+	        File app = new File(PropReader.getapkFilePath()); 
+	        DesiredCapabilities cap = new DesiredCapabilities();	        
+			cap.setCapability("deviceName", PropReader.getDeviceName());
+			cap.setCapability("platformVersion", PropReader.getPlatformVersion());
+			cap.setCapability("platformName", PropReader.getPlatformName());
+			cap.setCapability("udid", PropReader.getUDID());
+			cap.setCapability("app", app.getAbsolutePath());
+			cap.setCapability("noReset",PropReader.getNoReset());
+			driver = new AndroidDriver<MobileElement>(new URL(PropReader.getURL()), cap);	
+			
+			
+		}
+	
+  
+
+	public void logStatusFail(String description, String expected ,String actual ,String testStatus ) throws Exception {
+		
+		 folder_name="screenshot";
+		 File f=driver.getScreenshotAs(OutputType.FILE);
+		 df=new  SimpleDateFormat("dd-MMM-yyyy__hh_mm_ssaa");
+		 new File(folder_name).mkdir();
+		 String file_name=df.format(new Date())+Page_Helper.class.getName()+".png";
+		 FileHandler.copy(f, new File(folder_name + "/" + file_name));
+		 System.out.println("Screenshot is Captured");
+		 System.out.println(description);
+		 System.out.println(expected);
+		 System.out.println(actual);
+		 System.out.println(testStatus);
+		log.error("Status Failed : "+ description+ "~~~~" +actual+ "~~~~" +expected + "~~~~" +testStatus);
+	}
+		
+		public void logStatusPass(String description, String expected ,String actual ,String testStatus) {
+			System.out.println(description);
+			System.out.println(expected);
+			System.out.println(actual);
+			System.out.println(testStatus);
+			log.info("Status Passed  : "+ description+ "~~~~" +actual+ "~~~~" +expected + "~~~~" +testStatus);
+		}
+		
+		@AfterClass
+		public void tearDown() {
+			driver.quit();
+			
+		}	
+	
+	
+
+
+}
---
+++ src/com/androidreference/library/Operations.java
@@ -0,0 +1,151 @@
+package com.androidreference.library;
+import java.util.NoSuchElementException;
+
+import org.openqa.selenium.By;
+
+import com.anb.androidreference.Page_Elements.Attribute;
+import com.anb.androidreference.Page_Elements.LocatorType;
+
+import io.appium.java_client.MobileElement;
+
+public class Operations extends Base_Test{
+	
+	
+	MobileElement element;
+	
+	public MobileElement locateElement(String XPathString, LocatorType locateBy) {
+		System.out.println("Locating Element " + XPathString);
+		System.out.println(driver);
+		try {
+			switch (locateBy) {
+			case ID:
+				element = driver.findElement(By.id(XPathString));
+				break;
+			case XPATH:
+				element = driver.findElement(By.xpath(XPathString));
+			default:
+				break;
+			}
+			System.out.println("*************Element Located for XPathString " + XPathString);
+
+		} catch (NoSuchElementException e) {
+			if (element == null) {
+				System.out.println("*************Unable to get element for XPathString " + XPathString);
+			}
+
+			e.printStackTrace();
+		}
+		return element;
+
+	}
+
+	
+	public boolean getElementPresence(String XPathString, LocatorType locateBy) {
+		boolean found = true;
+		try {
+			element = locateElement(XPathString,locateBy);
+			if (null == element) {
+				System.out.println( "*************Element is not available for XPathString :"+XPathString);
+				found= false;
+
+			}
+		} catch (Exception e) {
+			System.out.println("*************Element is not available for XPathString " + XPathString);
+			found= false;	
+					}
+		return found;
+
+
+	}
+
+	public String getAttribute(String XPathString, LocatorType locateBy, Attribute attribute) {
+		String value = null;
+
+		try {
+			element = locateElement(XPathString, locateBy);
+			if (null == element) {
+				System.out.println(
+						Thread.currentThread().getName() + "*************Element is not available to fetch attribute ");
+				return "";
+
+			}
+		} catch (Exception e) {
+			System.out.println(Thread.currentThread().getName()
+					+ "*************Element is not available to fetch attribute " + XPathString);
+			return "";
+		}
+
+		if (attribute.toString() != null) {
+			while (true) {
+				try {
+					value = element.getAttribute(attribute.toString());
+					break;
+				} catch (Exception e) {
+					value = "";
+				}
+			}
+		} else {
+			System.out.println(
+					Thread.currentThread().getName() + "*************No Matchinhg Attribute found  " + attribute.toString());
+		}
+
+		if (value == null) {
+			value = "";
+			System.out.println(
+					Thread.currentThread().getName() + "*************Could not fetch value for the attribute ");
+		}
+		return value;
+
+	}
+	
+	
+	public void click(String XPathString, LocatorType locateBy) {
+
+
+		try {
+			element = locateElement(XPathString, locateBy);
+			if (null == element) {
+				System.out.println("*************Element is not available to Click ");
+			}
+			else {
+				try {
+					element.click();
+					System.out.println("*************Element is Clicked ");
+				}
+				catch(Exception e) {
+					System.out.println(
+							Thread.currentThread().getName() + "*************Click Operation failed for XPathString :  "+XPathString);
+					e.printStackTrace();
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(Thread.currentThread().getName()
+					+ "*************Element is not available to Click " + XPathString);
+		}
+
+		
+		return;
+
+	
+	}
+	
+	
+	public boolean isNotNullAndNotEmpty(String value) {
+		boolean result =false;
+		try {
+			if(null != value && !value.isEmpty()) {
+				result =true;
+			}
+			}
+		catch(Exception e) {
+			e.printStackTrace();
+			return result;
+		}
+		
+		return result;
+		
+	}
+	
+
+
+}
